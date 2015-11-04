9a999595ef897d60e7896b9bf3b12a8007352662
russell-stewart <rstewart16@kentdenver.org>
2015-11-04 12:21:42 -0700

    Add collisions and losing

--- canvas.js
+++ canvas.js
@@ -48,7 +48,7 @@ function loadCanvas() {
           context.fillRect(0 , 0 , 600 , 230);
           context.fillRect(0 , 500 , 600 , 100);
 
-          context.translate(0.005*start.getSeconds() , 0);
+          context.translate(0.01*start.getSeconds() , 0);
           context.drawImage(cloud1 , 100 , 100, 150, 100);
           context.drawImage(cloud2 , 300 , 500, 150, 100);
           context.drawImage(cloud3 , 0 , 500, 150, 100);
@@ -68,12 +68,13 @@ function loadCanvas() {
   }else alert('error');
 }
 
+var stillPlaying = true;
 var pigY = 200;
 var cloudX = 400;
 var cloudY = Math.floor((Math.random() * 600) + 1);
-var cloudX1 = 550;
+var cloudX1 = 600;
 var cloudY1 = Math.floor((Math.random() * 600) + 1);
-var cloudX2 = 700;
+var cloudX2 = 800;
 var cloudY2 = Math.floor((Math.random() * 600) + 1);
 
 function game() {
@@ -107,6 +108,9 @@ function game() {
     context.save();
     var intervalID = window.requestAnimationFrame(movePig());
     function movePig() {
+      if(pigY <= cloudY + 50 && pigY >= cloudY - 50 && cloudX <= 110 && cloudX >= 10) stillPlaying = false;
+      if(pigY <= cloudY1 + 50 && pigY >= cloudY1 - 50 && cloudX1 <= 110 && cloudX1 >= 10) stillPlaying = false;
+      if(pigY <= cloudY2 + 50 && pigY >= cloudY2 - 50 && cloudX2 <= 110 && cloudX2 >= 10) stillPlaying = false;
       var pig = new Image();
       var cloud1 = new Image();
       var cloud2 = new Image();
@@ -123,23 +127,25 @@ function game() {
           cloudY = Math.floor((Math.random() * 500) + 1);
         }
         context.drawImage(cloud1 , cloudX , cloudY , 150 , 100);
-
-
         if(cloudX1 > 0) cloudX1 -= 1;
         else {
           cloudX1 = 600;
           cloudY1 = Math.floor((Math.random() * 500) + 1);
         }
         context.drawImage(cloud2 , cloudX1 , cloudY1 , 150 , 100);
-
-
         if(cloudX2 > 0) cloudX2 -= 1;
         else {
           cloudX2 = 600;
           cloudY2 = Math.floor((Math.random() * 500) + 1);
         }
         context.drawImage(cloud3 , cloudX2 , cloudY2 , 150 , 100);
-      window.requestAnimationFrame(movePig());
+      if(stillPlaying) window.requestAnimationFrame(movePig());
+      else {
+        context.fillStyle = '#FFFFFF';
+        context.font = '80px OCR A Std';
+        context.textAlign = 'left';
+        context.fillText('Game over!' , 40 , 300);
+      }
       } , false);
       pig.src = 'superpig.png';
       cloud1.src = 'cloud1.png';
